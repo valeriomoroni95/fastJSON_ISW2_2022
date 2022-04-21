297846b87e19dd1546c6fc4f115b33eff58d8c34
valeriomoroni95 <valeriomoroni95@gmail.com>
2022-04-21 10:36:23 +0200

    Maven configuration added

---
+++ src/main/java/fastJSON_ISW2/App.java
@@ -0,0 +1,13 @@
+package fastJSON_ISW2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
